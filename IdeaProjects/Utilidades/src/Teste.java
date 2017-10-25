fd7101fde2653428e703686cdf36271e02f63bd7
Deivison Charles <charlesluttos@iCloud.com>
2017-10-25 20:53:59 -0300

    Mudança valor
    Teste de mudança

--- IdeaProjects/Utilidades/src/Teste.java
+++ IdeaProjects/Utilidades/src/Teste.java
@@ -1,6 +1,6 @@
 public class Teste {
     public static void main(String[] args){
-        String[] lista = {"9876","1423","3678","3210","3061"};
+        String[] lista = {"9877","1423","3678","3210","3061"};
         System.out.printf("%d ",ProcessaTexto.valoresMaisDigitados(lista));
     }
 }
