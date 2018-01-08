431b690ba53c4d5efc17b04322abc91da9506a8f
Deivison Charles <charlesluttos@iCloud.com>
2018-01-08 14:05:45 -0300

    Correção
    Excluindo arquivos sem utilidades

--- IdeaProjects/Utilidades/src/Teste.java
+++
@@ -1,6 +0,0 @@
-public class Teste {
-    public static void main(String[] args){
-        String[] lista = {"9877","1423","3678","3210","3061"};
-        System.out.printf("%d ",ProcessaTexto.valoresMaisDigitados(lista));
-    }
-}
--- PycharmProjects/MaxProduct(Challenge easy)/MaxProductFinder.py
+++
@@ -1,8 +0,0 @@
-
-
-def maxProductFinder(iList):
-    if (len(iList) > 2):
-        for item in iList:
-            
-
-maxProductFinder([1,3])
